b5827e691a6241c7f6a6f738acb01a079f0c4bba
Mohak Jain <mohakj500@gmail.com>
2022-07-12 16:06:46 +0530

    Bookmark File Created

---
+++ Bookmarks/Models/Bookmark.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text.Json;
+
+namespace Bookmarks.Models
+{
+    public class Bookmark
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
